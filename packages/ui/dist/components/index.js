373f9663274ca8c1c5eede2583065f69f747affb
everydaypanos <hi@everydaypanos.com>
2026-01-09 12:18:08 +0200

    ,

---
+++ packages/ui/dist/components/index.js
@@ -0,0 +1,6 @@
+/*
+ * This is a barrel export file for all components and their related types.
+ *
+ * 🟢 You can import this file directly.
+ */
+export * from "./v-keyboard";
---
+++ packages/ui/src/components/v-keyboard.ts
@@ -0,0 +1,1 @@
+export class VKeyboard extends HTMLElement {}

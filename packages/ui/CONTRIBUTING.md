25bc36d578fa362da3c1772f562ce6a3a687e1fa
everydaypanos <hi@everydaypanos.com>
2026-05-06 21:23:10 +0300

    ,

---
+++ packages/ui/CONTRIBUTING.md
@@ -0,0 +1,157 @@
+# Contributing
+
+Thanks for your interest in contributing to ui.shadcn.com. We’re happy to have you here.
+
+Please take a moment to review this document before submitting your first pull request. We also strongly recommend that you check for open issues and pull requests to see if someone else is working on something similar.
+
+If you need any help, feel free to reach out to [@everydaypanos](https://twitter.com/everydaypanos).
+
+## About this repository
+
+This repository is a monorepo.
+
+- We use [pnpm](https://npmjs.com) and [`workspaces`](https://docs.npmjs.com/cli/v8/using-npm/workspaces) for development.
+
+## Structure
+
+This repository is structured as follows:
+
+```
+apps
+├── docs
+└── v1
+packages
+├── ui
+│   ├── custom-elements
+│   ├── js
+│   │   ├── client
+│   │   └── components
+│   ├── scss
+│   └── web-components.html-data
+└── eslint-plugin
+```
+
+| Path                     | Description                              |
+| ------------------------ | ---------------------------------------- |
+| `apps/docs`              | The documentation website.               |
+| `apps/v1`                | The living standard app for development. |
+| `packages/ui`            | The `swiftwc` package.                   |
+| `packages/eslint-plugin` | The ESLint package.                      |
+
+## Development
+
+### Fork this repo
+
+You can fork this repo by clicking the fork button in the top right corner of this page.
+
+### Clone on your local machine
+
+```bash
+git clone https://github.com/your-username/ui.git
+```
+
+### Navigate to project directory
+
+```bash
+cd packages/ui
+```
+
+### Create a new Branch
+
+```bash
+git checkout -b my-new-branch
+```
+
+### Install dependencies
+
+```bash
+npm install -w @swiftwc/ui
+```
+
+#### Examples
+
+1. To run the `v1` website:
+
+```bash
+npm run dev -w @swiftwc/v1
+npm run dev:scss -w @swiftwc/v1
+```
+
+2. To run the `ui` package:
+
+```bash
+npm run build -w @swiftwc/ui
+```
+
+## Documentation
+
+The documentation for this project is located in the `docs` workspace. You can run the documentation locally by running the following command:
+
+```bash
+npm run dev -w @swiftwc/docs
+```
+
+Documentation is written using [VitePress](https://vitepress.dev). You can find the documentation files in the `apps/docs` directory.
+
+## Components
+
+We use a registry system for developing components. You can find the source code for the components under `packages/ui/js/components`. The styles for the components are under `packages/ui/scss/components`.
+
+```bash
+packages
+└── ui
+    ├── js
+    │   └── components
+    └── scss
+        └── components
+```
+
+When adding or modifying components, please ensure that:
+
+1. You make the changes for every style.
+2. You update the documentation.
+3. You run `npm run build:registry -w @swiftwc/ui` to update the registry.
+
+## Commit Convention
+
+Before you create a Pull Request, please check whether your commits comply with
+the commit conventions used in this repository.
+
+When you create a commit we kindly ask you to follow the convention
+`category(scope or module): message` in your commit message while using one of
+the following categories:
+
+- `feat / feature`: all changes that introduce completely new code or new
+  features
+- `fix`: changes that fix a bug (ideally you will additionally reference an
+  issue if present)
+- `refactor`: any code related change that is not a fix nor a feature
+- `docs`: changing existing or creating new documentation (i.e. README, docs for
+  usage of a lib or cli usage)
+- `build`: all changes regarding the build of the software, changes to
+  dependencies or the addition of new dependencies
+- `test`: all changes regarding tests (adding new tests or changing existing
+  ones)
+- `ci`: all changes regarding the configuration of continuous integration (i.e.
+  github actions, ci system)
+- `chore`: all changes to the repository that do not fit into any of the above
+  categories
+
+  e.g. `feat(components): add new prop to the avatar component`
+
+If you are interested in the detailed specification you can visit
+https://www.conventionalcommits.org/.
+
+## Requests for new components
+
+If you have a request for a new component, please open a discussion on GitHub. We’ll be happy to help you out.
+
+## Testing
+
+Tests are written using [Vitest](https://vitest.dev). You can run all the tests from the root of the repository.
+
+```bash
+npm run test -w @swiftwc/ui
+```
+
+Please ensure that the tests are passing when submitting a pull request. If you’re adding new features, please include tests.
--- packages/ui/scripts/make-manifest.ts
+++ packages/ui/scripts/make-manifest.ts
@@ -1,6 +1,5 @@
 import doctrine from 'doctrine'
 import { writeFileSync } from 'node:fs'
-import { mkdir, rm } from 'node:fs/promises'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { ArrayLiteralExpression, Project, SyntaxKind } from 'ts-morph'
@@ -82,66 +81,7 @@ interface VsHtmlDataTag {
 // https://github.com/microsoft/vscode-html-languageservice/blob/main/docs/customData.schema.json
 const vscode: VsHtmlDataV1 = {
   version: 1.1,
-  tags: [
-    // {
-    //   name: 'v-keyboard',
-    //   description: 'Required for all.\n\nAdd a `<v-keyboard>` element inside your body markup.',
-    //   references: [
-    //     {
-    //       name: 'GitHub',
-    //       url: 'https://github.com/github/time-elements#relative-time',
-    //     },
-    //     {
-    //       name: 'WebComponents',
-    //       url: 'https://www.webcomponents.org/element/time-elements#relative-time',
-    //     },
-    //     {
-    //       name: 'NPM',
-    //       url: 'https://www.npmjs.com/package/@github/time-elements#relative-time',
-    //     },
-    //   ],
-    // },
-    // {
-    //   name: 'label-view',
-    //   description: 'title',
-    //   references: [
-    //     {
-    //       name: 'GitHub',
-    //       url: 'https://github.com/github/time-elements#time-until',
-    //     },
-    //     {
-    //       name: 'WebComponents',
-    //       url: 'https://www.webcomponents.org/element/time-elements#time-until',
-    //     },
-    //     {
-    //       name: 'NPM',
-    //       url: 'https://www.npmjs.com/package/@swiftwc/ui/generated/components/label-view.js',
-    //     },
-    //   ],
-    //   attributes: [
-    //     {
-    //       name: 'title',
-    //       description: 'Value Type: ISO 8601 date.\nDescription: Required date of element such as `2014-06-01T13:05:07Z`',
-    //       references: [
-    //         {
-    //           name: 'GitHub',
-    //           url: 'https://github.com/github/time-elements#options',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       name: 'line-limit',
-    //       description: 'Value Type: ISO 8601 date.\nDescription: Required date of element such as `2014-06-01T13:05:07Z`',
-    //       references: [
-    //         {
-    //           name: 'GitHub',
-    //           url: 'https://github.com/github/time-elements#options',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-  ],
+  tags: [],
   globalAttributes: [
     {
       name: 'frame:width',
@@ -194,45 +134,21 @@ const elements: CustomElementDataV2 = {
   modules: [],
 }
 
-const mds: [string, string][] = []
-
-// interface MetaType {
-//   element?: string
-//   description?: string
-//   summary?: string
-// }
-
-// const extr = (jsDoc?: JSDoc) => {
-//   const meta: MetaType = {
-//     description: jsDoc?.getComment()?.toString() ?? '',
-//   }
-//   for (const tag of jsDoc?.getTags() ?? []) {
-//     const name = tag.getTagName(),
-//       value = tag?.getComment() ?? ''
-//     if (!meta[name]) meta[name] = value
-//     else {
-//       // handle repeated tags like @param, @event
-//       if (!Array.isArray(meta[name])) meta[name] = [meta[name]]
-//       meta[name].push(value)
-//     }
-//   }
-
-//   return meta
-// }
-
 for (const sourceFile of project.getSourceFiles()) {
   const classes = sourceFile.getClasses()
 
   for (const cls of classes) {
+    const is = kebabCase(`${cls.getName()}`)
+
     const module: CustomElementModule = {
       kind: 'javascript-module',
-      path: `./generated/components/${kebabCase(`${cls.getName()}`)}.ts`,
+      path: `./generated/components/${is}.ts`,
       declarations: [
         {
           kind: 'class',
           customElement: true,
           name: `${cls.getName()}`,
-          tagName: kebabCase(`${cls.getName()}`),
+          tagName: is,
           description: '',
           superclass: {
             name: cls.getBaseClass()?.getName()?.replaceAll('Base', 'HTMLElement')?.replaceAll('Associated', '') ?? 'HTMLElement',
@@ -249,7 +165,7 @@ for (const sourceFile of project.getSourceFiles()) {
         },
         {
           kind: 'custom-element-definition',
-          name: kebabCase(`${cls.getName()}`),
+          name: is,
           declaration: {
             name: `${cls.getName()}`,
           },
@@ -267,34 +183,14 @@ for (const sourceFile of project.getSourceFiles()) {
         m.getName() !== 'observedAttributes'
       )
     })) {
-      module.declarations[0].members ??= []
-      module.declarations[0].members.push({
+      ;(module.declarations[0].members ??= []).push({
         kind: 'field',
         name: m.getName(),
-        // type: {
-        //   text: 'boolean',
-        // },
-        // description: 'disabled state',
-        // default: 'true',
-        // kind2: m.getKindName(),
-        // returnType2: m.getReturnType?.()?.getText?.(),
       })
-
-      // const classNames = [
-      //   ...m
-      //     .getReturnType?.()
-      //     ?.getText?.()
-      //     .matchAll(/\.([A-Z]\w*)/g),
-      // ].map((match) => match[1])
-
-      // mds.push([
-      //   kebabCase(`${cls.getName()}`),
-      //   `\`${m.getName()}\`\n\n: ${'GetAccessor' === m.getKindName() ? '<Badge type="warning" text="readonly" />' : ''} ${0 < classNames.length ? classNames.join(' | ') : m.getReturnType?.()?.getText?.()}`,
-      // ])
     }
 
     const row: VsHtmlDataTag = {
-      name: kebabCase(`${cls.getName()}`),
+      name: is,
     }
 
     const leading = cls
@@ -319,9 +215,7 @@ for (const sourceFile of project.getSourceFiles()) {
 
           case 'event':
           case 'fires':
-            module.declarations[0].events ??= []
-
-            module.declarations[0].events.push({
+            ;(module.declarations[0].events ??= []).push({
               name: tag.title,
               type: {
                 text: 'Event',
@@ -330,9 +224,7 @@ for (const sourceFile of project.getSourceFiles()) {
 
             continue
           case 'slot':
-            module.declarations[0].slots ??= []
-
-            module.declarations[0].slots.push({
+            ;(module.declarations[0].slots ??= []).push({
               name: tag.title,
               description: tag.description ?? undefined,
             })
@@ -342,19 +234,7 @@ for (const sourceFile of project.getSourceFiles()) {
       }
     }
 
-    // const tag =
-    //   cls
-    //     .getDecorators()
-    //     .find((d) => d.getName() === 'customElement')
-    //     ?.getArguments()[0]
-    //     ?.getText()
-    //     ?.replace(/['"`]/g, '') ?? meta?.element
-
-    // const staticProp = cls.getStaticProperty('observedAttributes')
-    const getter = cls.getGetAccessor('observedAttributes') // cls.getStaticMethod?.('observedAttributes')
-
-    const returnStmt = getter?.getDescendantsOfKind(SyntaxKind.ReturnStatement).at(0)
-    const expr = returnStmt?.getExpression()
+    const expr = cls.getGetAccessor('observedAttributes')?.getDescendantsOfKind(SyntaxKind.ReturnStatement).at(0)?.getExpression()
 
     if (expr instanceof ArrayLiteralExpression) {
       for (const prop of expr.getElements()) {
@@ -386,16 +266,13 @@ for (const sourceFile of project.getSourceFiles()) {
           }
         }
 
-        row.attributes ??= []
-        row.attributes.push(attr)
-
-        module.declarations[0].attributes ??= []
-        module.declarations[0].attributes.push({ name: attr.name })
+        ;(row.attributes ??= []).push(attr)
+        //
+        ;(module.declarations[0].attributes ??= []).push({ name: attr.name })
       }
     }
 
-    vscode.tags ??= []
-    vscode.tags.push(row)
+    ;(vscode.tags ??= []).push(row)
 
     elements.modules.push(module)
   }
@@ -404,11 +281,3 @@ for (const sourceFile of project.getSourceFiles()) {
 writeFileSync(resolve(__dirname, '../web-components.html-data/en.json'), JSON.stringify(vscode, null, 2))
 
 writeFileSync(resolve(__dirname, '../custom-elements/en.json'), JSON.stringify(elements, null, 2))
-
-const reflections = resolve(__dirname, `../../../apps/docs/reflections`)
-
-await rm(reflections, { recursive: true, force: true })
-
-await mkdir(reflections, { recursive: true })
-
-for (const [k, md] of mds) writeFileSync(resolve(__dirname, `${reflections}/${k}.md`), md, {})
